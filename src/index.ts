b507b2119623583c074facb4f3234c5a6c483e79
Ishwar Suthar <ishwar16suthar@gmail.com>
2026-04-15 23:48:34 +0530

    style: format code for consistency and readability in index.ts and tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,47 @@
 import express from "express";
-import * as trpcExpress from '@trpc/server/adapters/express';
+import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./server/index.js";
-import fs from 'fs/promises'
-import { createContext } from './server/context.js'
-import { generateOpenApiDocument , createOpenApiExpressMiddleware} from 'trpc-to-openapi';
+import fs from "fs/promises";
+import { createContext } from "./server/context.js";
+import {
+  generateOpenApiDocument,
+  createOpenApiExpressMiddleware,
+} from "trpc-to-openapi";
 
+const app = express();
 
-const app = express()
-
-app.use(express.json())
+app.use(express.json());
 
 const openapiDocument = generateOpenApiDocument(appRouter, {
-    baseUrl: 'http://localhost:8000/api',
-    title:'My TODO Server',
-    version:'1.0.0'
-})
+  baseUrl: "http://localhost:8000/api",
+  title: "My TODO Server",
+  version: "1.0.0",
+});
 
-fs.writeFile('./openapi-specification.json', JSON.stringify(openapiDocument))
+fs.writeFile("./openapi-specification.json", JSON.stringify(openapiDocument));
 
-app.get('/',(req,res)=>{
-    return res.json({status:'Server running'})
-})
+app.get("/", (req, res) => {
+  return res.json({ status: "Server running" });
+});
 
-app.get('/openapi.json',(req,res)=>{
-    return res.json(openapiDocument)
-})
-
-app.use('/api', createOpenApiExpressMiddleware({
-    router:appRouter,
-    createContext
-}))
+app.get("/openapi.json", (req, res) => {
+  return res.json(openapiDocument);
+});
 
 app.use(
-  '/trpc',
+  "/api",
+  createOpenApiExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+);
+
+app.use(
+  "/trpc",
   trpcExpress.createExpressMiddleware({
     router: appRouter,
     createContext,
   }),
-)
+);
 
-app.listen(8000,()=> console.log(`Express server is running on port 8000`))
+app.listen(8000, () => console.log(`Express server is running on port 8000`));
