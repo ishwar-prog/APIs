d65cd2cc1fc3eef9257a42f71e7dc7488e3a769b
Ishwar Suthar <ishwar16suthar@gmail.com>
2026-04-15 23:47:20 +0530

    style: format code for consistency and readability in todo models and routes

--- src/server/routes/todo/todo.routes.ts
+++ src/server/routes/todo/todo.routes.ts
@@ -1,37 +1,48 @@
-import { z } from 'zod'
-import {router , publicProcedure } from '../../trpc.js'
-import { getAllTodosOutputModel, todoModel, type Todo } from './models.js'
+import { z } from "zod";
+import { router, publicProcedure } from "../../trpc.js";
+import { getAllTodosOutputModel, todoModel, type Todo } from "./models.js";
 
-const TODOS: Todo[] = [{id:'1',isCompleted:false, title:'Record Video',description:'I need to record a video on prod grade apis'}]
+const TODOS: Todo[] = [
+  {
+    id: "1",
+    isCompleted: false,
+    title: "Record Video",
+    description: "I need to record a video on prod grade apis",
+  },
+];
 
 export const todoRouter = router({
-    createTodo:publicProcedure.meta({
-        openapi:{
-            method:'POST',
-            path:'/create-todo',
-            tags:['Todo'],
-            description:'Creates a new todo item'
-        }
-    }).input(z.object({title:z.string()})).output(z.object({todo:todoModel})).mutation(({input})=>{
-        TODOS.push({id:'123', isCompleted:false, title:input.title})
-        return{
-            todo:{id:'123', isCompleted:false, title:input.title}
-        }
-    }),
-    getAllTodos:publicProcedure
+  createTodo: publicProcedure
     .meta({
-        openapi:{
-            method:'GET',
-            path:'/todos',
-            tags:['Todo'],
-            description:'Returns all the todos',
-        }
+      openapi: {
+        method: "POST",
+        path: "/create-todo",
+        tags: ["Todo"],
+        description: "Creates a new todo item",
+      },
+    })
+    .input(z.object({ title: z.string() }))
+    .output(z.object({ todo: todoModel }))
+    .mutation(({ input }) => {
+      TODOS.push({ id: "123", isCompleted: false, title: input.title });
+      return {
+        todo: { id: "123", isCompleted: false, title: input.title },
+      };
+    }),
+  getAllTodos: publicProcedure
+    .meta({
+      openapi: {
+        method: "GET",
+        path: "/todos",
+        tags: ["Todo"],
+        description: "Returns all the todos",
+      },
     })
     .input(z.undefined())
     .output(getAllTodosOutputModel)
-    .query(()=>{
-        return {
-            todos: TODOS
-        }
-    })
-})
+    .query(() => {
+      return {
+        todos: TODOS,
+      };
+    }),
+});
